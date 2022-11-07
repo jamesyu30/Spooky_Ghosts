cdd0a45b1127a7df728a66e54844b9725d309904
jamesyu30 <jyu30@stuy.edu>
2022-11-07 18:10:44 -0500

    login and register page

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask, session, render_template, request, redirect
 import os
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)  #set up the session with a secret key
 app.config['s'] = app.secret_key
 user = "a"
 passw = "1"
+
+DB_FILE="accounts.db"            
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()
 
 @app.route("/")
 def home():
@@ -18,6 +23,9 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login(): #checks if inputs match
     if request.method == "POST":
+        c.execute("SELECT username FROM accounts")
+        user1 = c.fetchone()
+        print(user1[0])
         if user != request.form['username']: #if username doesn't match
             return "Invalid username"
         if passw != request.form['password']: #if password doesn't match
@@ -32,7 +40,21 @@ def register():
 
 @app.route("/create", methods=['GET', 'POST'])
 def verify():
-    return "test"
+    c.execute("DROP TABLE if exists accounts")
+    command = f"CREATE TABLE accounts (username TEXT, password TEXT);"          # test SQL stmt in sqlite3 shell, save as string
+    c.execute(command) 
+    insert = f"INSERT INTO accounts VALUES (\"{request.form['createusername']}\", \"{request.form['createpassword']}\")"
+    c.execute(insert)
+    db.commit()
+    c.execute("SELECT * FROM accounts")
+    rows = c.fetchall() #prints table
+    table = ""
+    for row in rows:
+        row2 = "".join(row)
+        print(row)
+        table = table+row2
+    #db.close()
+    return table 
 """
 @app.route("/auth", methods=['GET', 'POST'])
 def auth():
