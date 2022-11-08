b61ae5036f314cb25da299d62054fec060beec4f
jamesyu30 <jyu30@stuy.edu>
2022-11-07 20:23:56 -0500

    login and register page

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,16 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = os.urandom(32)  #set up the session with a secret key
 app.config['s'] = app.secret_key
-user = "a"
-passw = "1"
 
 DB_FILE="accounts.db"            
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()
-
+c.execute("DROP TABLE if exists accounts")
+command = f"CREATE TABLE accounts (username TEXT, password TEXT);"
+c.execute(command)
+    
 @app.route("/")
 def home():
-    #session['username'] = "a"
     if not 'username' in session: #checks if session is empty
         return render_template('login.html')
     else:
@@ -23,49 +23,47 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login(): #checks if inputs match
     if request.method == "POST":
-        c.execute("SELECT username FROM accounts")
-        user1 = c.fetchone()
-        print(user1[0])
-        if user != request.form['username']: #if username doesn't match
-            return "Invalid username"
-        if passw != request.form['password']: #if password doesn't match
-            return "Invalid password"
-        else:
-            session['username'] = request.form['username'] #if they both match, create a session
-            return redirect("/auth", code=307) #code 307 makes the redirect make a post request
+        c.execute("SELECT * FROM accounts")
+        accdata = c.fetchall()
+        for data in accdata:
+            if(data[0] == request.form['username'] and data[1] == request.form['password']):
+                session['username'] = request.form['username'] #if they both match, create a session
+                return redirect("/auth", code=307) #code 307 makes the redirect make a post request
+        return "Invalid username and/or password"
 
+           
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
 
 @app.route("/create", methods=['GET', 'POST'])
 def verify():
-    c.execute("DROP TABLE if exists accounts")
-    command = f"CREATE TABLE accounts (username TEXT, password TEXT);"          # test SQL stmt in sqlite3 shell, save as string
-    c.execute(command) 
     insert = f"INSERT INTO accounts VALUES (\"{request.form['createusername']}\", \"{request.form['createpassword']}\")"
     c.execute(insert)
     db.commit()
+    """ PRINTS TABLE
     c.execute("SELECT * FROM accounts")
-    rows = c.fetchall() #prints table
+    rows = c.fetchall() 
     table = ""
     for row in rows:
         row2 = "".join(row)
-        print(row)
+       # print(row)
         table = table+row2
     #db.close()
-    return table 
-"""
+    """
+    return redirect("/")
+
 @app.route("/auth", methods=['GET', 'POST'])
 def auth():
-    return "<h1>Welcome, " + session['username'] +"</h1><br>" + render_template('response.html')
+    return "<h1>Welcome, " + session['username'] +"</h1><br>" + render_template('landing.html')
     #post request makes session['username'] not return an error on refresh
 
 @app.route("/logout")
 def logout():
     session.pop('username') #gets rid of session
     return redirect("/")#goes home
-"""
+
+
 if __name__ == "__main__": 
-    app.debug = True 
+    app.debug = True
     app.run()    
